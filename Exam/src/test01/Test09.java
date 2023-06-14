532788a2092dee0e70ad9e3d7a45f84092cbb89d
bllor <duki0105@gmail.com>
2023-06-14 11:39:33 +0900

    연습문제

--- Exam/src/test01/Test09.java
+++ Exam/src/test01/Test09.java
@@ -1,5 +1,29 @@
 package test01;
-
+/*
+ * 날짜: 2023/06/14
+ * 이름: 최동일
+ * 내용: 자바 반복문 연습문제
+ */
 public class Test09 {
-
+	public static void main(String[] args) {
+	
+		int count= 0;
+		for(int i=1; i<=9; i++) {
+			if(i<=5){
+				count=i;
+			}else {
+				count=10-i;
+			}
+			
+			for(int j=1; j<=5-count;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int k=1; k<=2*count-1;k++) {
+				System.out.print("*");
+			}
+			
+			System.out.print("\n");
+		}
+}
 }
