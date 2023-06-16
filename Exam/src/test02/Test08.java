fb68e48e649f3e0676650effae9f13d61f8210e8
bllor <duki0105@gmail.com>
2023-06-16 09:55:37 +0900

    수업 파일

--- Exam/src/test02/Test08.java
+++ Exam/src/test02/Test08.java
@@ -18,6 +18,15 @@ public class Test08 {
 		if(n<=1) {
 			return 1;
 		}
-		return n*factorial((n-1));
+		return n*factorial((n-1));//<-재귀함수(=재귀메서드) recursive
 	}
+	//현대 사회에서는 재귀함수를 권하지 않는다.
+/*
+ * fcatorial(n=3)일 때
+ * n!=3이므로
+ * return 3*factorial(2)가 되는데 factorial(2)를 수행
+ * factorial(2) = 2*factorial(1) 
+ * factorial(1) = factorial(1) =1
+ * 3*factorial(2)=3*2*factorial(1)=3*2*1
+ */
 }
