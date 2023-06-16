686a98b8a8f21d3b67fc246b5faa44676c723a57
bllor <duki0105@gmail.com>
2023-06-16 10:19:43 +0900

    코딩테스트

---
+++ CodingTest/src/step01/Test01.java
@@ -0,0 +1,8 @@
+package step01;
+//백준에 제출할 때 Class까지 복사해서 붙여넣기 한 뒤, class name을 main으로 바꿔줌
+public class Test01 {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+}
