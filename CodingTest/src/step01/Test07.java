e9e0e492ce47e29a9253f9479cbc0ff77cee66d5
bllor <duki0105@gmail.com>
2023-06-21 17:52:19 +0900

    test6

--- CodingTest/src/step01/Test07.java
+++ CodingTest/src/step01/Test07.java
@@ -1,5 +1,18 @@
 package step01;
 
+import java.util.Scanner;
+
 public class Test07 {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		String s= sc.next();
+		if(s=equal"joonas") {
+			System.out.println(s+"??!");
+		}
+		
+		sc.close();
+		equals
+	}
 }
