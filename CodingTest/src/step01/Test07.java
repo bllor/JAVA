76e0fea71277962c65b8c5dcd98caf3779b35d98
bllor <duki0105@gmail.com>
2023-06-22 11:34:25 +0900

    test7

--- CodingTest/src/step01/Test07.java
+++ CodingTest/src/step01/Test07.java
@@ -7,7 +7,7 @@ public class Test07 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println();//위의 문장 때문에 출력형식 에러가 났음
 		String s= sc.next();
 		String x="??!";
 		String r1= s+x;
