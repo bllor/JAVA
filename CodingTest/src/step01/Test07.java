f850d99c650f00ac766cf5a5138829025e267a91
bllor <duki0105@gmail.com>
2023-06-22 10:03:13 +0900

    test7끝

--- CodingTest/src/step01/Test07.java
+++ CodingTest/src/step01/Test07.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class Test07 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println();
-		String s= sc.next();
-		if(s=equal"joonas") {
-			System.out.println(s+"??!");
-		}
 		
+		Scanner sc = new Scanner(System.in);
+		
+		String s= sc.next();
+		String x="??!";
+		String r1= s+x;
+				
+		System.out.println(r1);
 		sc.close();
-		equals
+		
 	}
 }
