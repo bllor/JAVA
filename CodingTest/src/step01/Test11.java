a14a209a9dedb5a5674d4789a8c8618fc0a71aaa
bllor <duki0105@gmail.com>
2023-06-23 11:08:02 +0900

    백준1단계끝

--- CodingTest/src/step01/Test11.java
+++ CodingTest/src/step01/Test11.java
@@ -1,5 +1,20 @@
 package step01;
+/*
+ * 날짜: 2023/06/23
+ * 이름: 최동일
+ * 내용: 백준 1단계 11번 문제
+ */
+import java.util.Scanner;
 
 public class Test11 {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Long x = sc.nextLong();
+		Long y = sc.nextLong();
+		Long z = sc.nextLong();
+		
+		
+		System.out.println(x+y+z);
+	}
 }
