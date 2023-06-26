220c44ba9190eacf4614c7006607c7df48a6c166
bllor <duki0105@gmail.com>
2023-06-26 12:31:41 +0900

    예외처리

---
+++ Ch06/src/sub01/Shark.java
@@ -0,0 +1,16 @@
+package sub01;
+
+public class Shark extends Animal {
+
+	@Override
+	public void move() {
+		
+		System.out.println("Shark swim...");
+	}
+	
+	@Override
+	public void hunt() {
+		System.out.println("Shark Hunt...");
+	}
+	
+}
