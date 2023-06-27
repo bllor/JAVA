d75272785fcb84450cbcafb9e924e3b5c887938c
bllor <duki0105@gmail.com>
2023-06-27 17:50:01 +0900

    test2 2번

--- CodingTest/src/step02/Test02.java
+++ CodingTest/src/step02/Test02.java
@@ -1,5 +1,28 @@
 package step02;
+/*
+ * 날짜: 2023/06/27
+ * 이름: 최동일
+ * 내용: 백준 2단계 2번 문제
+ */
+import java.util.Scanner;
+
 
 public class Test02 {
 
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		if(x>=90) {
+			System.out.println("A");
+		}else if(x>=80) {
+			System.out.println("B");
+		}else if(x>=70) {
+			System.out.println("C");
+		}else if(x>=60) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+	}
 }
