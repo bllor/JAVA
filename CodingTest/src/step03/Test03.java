efcf300cb7f35ac8460a534e698031214004160a
bllor <duki0105@gmail.com>
2023-07-09 14:52:53 +0900

    3단계3번

---
+++ CodingTest/src/step03/Test03.java
@@ -0,0 +1,20 @@
+package step03;
+
+import java.util.Scanner;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int sum=0;
+		for(int i = n;i>0;i--) {
+			sum+=i;
+		}
+		System.out.println(sum);			
+		
+		sc.close();
+	}
+}
