16393f1f3bf0ef39fad1d5cdf6477439152938c1
bllor <duki0105@gmail.com>
2023-07-18 14:11:25 +0900

    연습문제

---
+++ Exam/src/test06/Test4.java
@@ -0,0 +1,21 @@
+package test06;
+
+/*
+ * 날짜: 2023/07/18
+ * 이름: 최동일
+ * 내용: 자바 총정리 연습문제
+ */
+public class Test4 {
+	
+	public static void main(String[] args) {
+		System.out.printf("%7s %10s %5s %6s\n", "10진수","2진수","8진수","16진수");
+		
+		for(int num =1; num<=128; num++) {
+			System.out.printf("%7s %10s %5s %6s\n",num,
+													Integer.toBinaryString(num),
+													Integer.toOctalString(num),
+													Integer.toOctalString(num));
+		}
+	}
+
+}
